969b6acebc3bed2db69ff9d81bd838c1d1edd5aa
diego-lucas-silva <silva.diegolucas@gmail.com>
2022-03-19 01:27:15 -0300

    Feito aula 01 - parte II -  até 01:56:53 (com erro) - semana-js-expert by erick wendel

--- tests/unit/public/routes.test.js
+++ tests/unit/public/routes.test.js
@@ -1,6 +1,11 @@
-import { jest, expect, describe, test } from '@jest/globals'
+import { jest, expect, describe, test, beforeEach } from '@jest/globals'
 
-describe('#Routes', () => {
+describe('#Routes - test site for API response', () => {
+  beforeEach(() => {
+    jest.resetAllMocks()
+    jest.clearAllMocks()
+  })
+
   test('my test', () => {
     expect(true).toBeTruthy()
   })
